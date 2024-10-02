a2b32fe6d09f4475989372ef30403ac92b82c894
Ernesto Cruz <ecruzolivera@gmail.com>
2024-10-02 13:19:31 -0600

    update

--- install/desktop/set-gnome-theme.sh
+++ install/desktop/set-gnome-theme.sh
@@ -1,9 +1,11 @@
 # install arc theme
 sudo apt install -y arc-theme
 
-sudo add-apt-repository -y ppa:papirus/papirus
-sudo apt update
-sudo apt install -y qt6-style-kvantum qt5-style-kvantum papirus-icon-theme
+if ! dpkg -l | grep -q papirus-icon-theme; then
+  sudo add-apt-repository -y ppa:papirus/papirus
+  sudo apt update
+  sudo apt install -y qt6-style-kvantum qt5-style-kvantum papirus-icon-theme
+fi
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
--- install/terminal/gdu.sh
+++ install/terminal/gdu.sh
@@ -1,3 +1,5 @@
-sudo add-apt-repository -y ppa:daniel-milde/gdu
-sudo apt-get update
-sudo apt-get install -y gdu
+if ! dpkg -l | grep -q gdu; then
+  sudo add-apt-repository -y ppa:daniel-milde/gdu
+  sudo apt-get update
+  sudo apt-get install -y gdu
+fi
