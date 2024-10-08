5a2cb725ccf5c9ef8f5105737b43242832d896f4
Ernesto Cruz <ecruzolivera@gmail.com>
2024-10-08 13:15:40 -0600

    adding pnpm adding ufw rules

--- install/terminal/ufw.sh
+++ install/terminal/ufw.sh
@@ -1,4 +1,9 @@
 sudo apt install -y ufw gufw
 sudo ufw enable
+
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+
+sudo ufw allow in on lxdbr0
+sudo ufw route allow in on lxdbr0
+sudo ufw route allow out on lxdbr0
