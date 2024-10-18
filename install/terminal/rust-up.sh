de4d1dd213a9e31fa3881227c926cad678ea1771
Ernesto Cruz <ecruzolivera@gmail.com>
2024-10-18 10:48:00 -0600

    adding rust analyzer

--- install/terminal/rust-up.sh
+++ install/terminal/rust-up.sh
@@ -1,6 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+rustup component add rust-analyzer
+
 # Probe RS
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
-
 sudo wget https://probe.rs/files/69-probe-rs.rules -P /etc/udev/rules.d/
