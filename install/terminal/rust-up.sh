67f3be5d5b67afc52f188a4cd85086f18f27a982
Ernesto Cruz <ecruzolivera@gmail.com>
2024-10-18 10:31:12 -0600

    adding probe rs

--- install/terminal/rust-up.sh
+++ install/terminal/rust-up.sh
@@ -1,1 +1,6 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Probe RS
+curl --proto '=https' --tlsv1.2 -LsSf https://github.com/probe-rs/probe-rs/releases/latest/download/probe-rs-tools-installer.sh | sh
+
+sudo wget https://probe.rs/files/69-probe-rs.rules -P /etc/udev/rules.d/
